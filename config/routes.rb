abb46c513371bd67f824abdbba910fa936122d9f
drs-smellie <makes.melly@gmail.com>
2013-12-10 17:04:27 +0100

    added sign up and sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Blocmarks::Application.routes.draw do
 
-  get "sessions/new"
   resources :users
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  resources :sessions
 
   resources :topics do
     resources :posts, except: [:index], controller: 'topics/posts' do
