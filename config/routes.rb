43fac3eb0e8aaf9343a7195cce7ce35704229bb5
drs-smellie <makes.melly@gmail.com>
2013-12-12 13:25:31 +0100

    added auto-login after sign-up and jumbotron

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Blocmarks::Application.routes.draw do
 
-  resources :users
-  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "sign_up" => "users#new", :as => "sign_up"
   get "log_in" => "sessions#new", :as => "log_in"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+
   resources :sessions
 
+  resources :users
   resources :topics do
     resources :posts, except: [:index], controller: 'topics/posts' do
     end
